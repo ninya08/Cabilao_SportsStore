622a79433e641e7a19ea12e09928b2ed96291ffe
ninya08 <126835023+ninya08@users.noreply.github.com>
2023-03-03 10:33:20 +0800

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+namespace Cabilao_SportsStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
---
+++ Models/EFStoreRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace Cabilao_SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDbContext context;
+
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IQueryable<Product> Products => context.Products;
+
+    }
+}
